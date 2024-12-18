16b5da6c693eea7c4b59d413a79812a274d1979f
Clay-KoSaking <2974107908@qq.com>
2024-12-18 16:38:30 +0800

    Change backend URL

--- web/src/api/axios.ts
+++ web/src/api/axios.ts
@@ -1,4 +1,4 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8088'
+axios.defaults.baseURL = 'https://bug.otmdb.cn:8088'
 axios.defaults.withCredentials = true
 export const $axios = axios
